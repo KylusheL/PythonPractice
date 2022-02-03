80a240921516fd817565ff03f905fc679940ab97
이의진 <ez2dj1010@naver.com>
2022-02-03 14:48:34 +0900

    Update 1이 될 때까지.py

--- 이코테/그리디/1이 될 때까지.py
+++ 이코테/그리디/1이 될 때까지.py
@@ -18,6 +18,7 @@ print(result)
 
 # 2. 책의 풀이
 
+# 1)
 # N, K을 공백을 기준으로 구분하여 입력 받기
 n, k = map(int, input().split())
 result = 0
@@ -39,7 +40,30 @@ while n > 1:
 
 print(result)
 
+# 2)
+# N, K공백을 기준으로 구분하여 입력 받기
+n, k = map(int, input().split())
+
+result = 0
+
+while True:
+    # N이 K로 나누어 떨어지는 수가 될 때까지만 1씩 빼기
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+    # N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
+        break
+    # K로 나누기
+    result += 1
+    n //= k
+
+# 마지막으로 남은 수에 대하여 1씩 빼기
+result += (n - 1)
+print(result)
+
 # 3. 얻어갈 점
 '''
 다양한 사이드 케이스를 고려할 것. (마감 처리 신중)
+(n // k) * k 를 통해 나머지를 쳐낼 수 있다.
 '''
