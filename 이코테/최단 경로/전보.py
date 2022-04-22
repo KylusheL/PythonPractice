ed12ccaf6eea430013dcc8332d1c80f09e79aca4
이의진 <ez2dj1010@naver.com>
2022-04-23 03:25:24 +0900

    Create 전보.py

---
+++ 이코테/최단 경로/전보.py
@@ -0,0 +1,95 @@
+# 1. 나의 초견
+import sys
+import heapq
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, c = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+dist = [INF] * (n + 1)
+
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    dist[start] = 0
+    while q:
+        cost, now = heapq.heappop(q)
+        if dist[now] < cost:
+            continue
+        for next, time in graph[now]:
+            if dist[now] + time < dist[next]:
+                dist[next] = dist[now] + time
+                heapq.heappush(q, (dist[next], next))
+
+dijkstra(c)
+send_cnt = 0
+max_time = 0
+for elem in dist[1:]:
+    if elem == INF or elem == 0:
+        continue
+    send_cnt += 1
+    max_time = max(max_time, elem)
+
+print(send_cnt, max_time)
+
+# 2. 책의 풀이
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수, 간선의 개수, 시작 노드를 입력받기
+n, m, start = map(int, input().split())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n + 1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    # X번 노드에서 Y번 노드로 가는 비용이 Z라는 의미
+    graph[x].append((y, z))
+
+def dijkstra(start):
+   q = []
+   # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
+   heapq.heappush(q, (0, start))
+   distance[start] = 0
+   while q: # 큐가 비어있지 않다면
+        # 가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+# 다익스트라 알고리즘을 수행
+dijkstra(start)
+
+# 도달할 수 있는 노드의 개수
+count = 0
+# 도달할 수 있는 노드 중에서, 가장 멀리 있는 노드와의 최단 거리
+max_distance = 0
+for d in distance:
+    # 도달할 수 있는 노드인 경우
+    if d != 1e9:
+        count += 1
+        max_distance = max(max_distance, d)
+
+# 시작 노드는 제외해야 하므로 count - 1을 출력
+print(count - 1, max_distance)
+
+# 3. 얻어갈 점
+'''
+다익스트라 알고리즘이 손에 익도록 하면 좋을 것 같다.
+'''
