db9265e1bb4d48ac0faa2cec8a3708a50addb398
이의진 <ez2dj1010@naver.com>
2022-06-16 16:14:44 +0900

    Create 서강그라운드(14938, G4).py

---
+++ Daily PS/BOJ/서강그라운드(14938, G4).py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, r = map(int, input().split())
+item = [0] + list(map(int, input().split()))
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    graph[i][i] = 0
+for _ in range(r):
+    a, b, l = map(int, input().split())
+    graph[a][b] = l
+    graph[b][a] = l
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+result = 0
+for i in range(1, n + 1):
+    tmp = 0
+    for j in range(1, n + 1):
+        if graph[i][j] <= m:
+            tmp += item[j]
+    result = max(result, tmp)
+
+print(result)
